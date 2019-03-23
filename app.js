f969fe6c7813da3089add0282d6905677f1cfc00
Asiapenguin <nicholaskong@live.ca>
2019-03-23 01:45:22 -0700

    /mapRegion

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const {mapRegions} = require("./mapRegion");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors({credentials: true, origin: true}));
+
+app.get("/", (req, res) => res.send("Hello World!"))
+
+app.get("/mapRegion", (req, res) => {
+  res.json({ data: mapRegions });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ mapRegion.js
@@ -0,0 +1,43 @@
+const mapRegions = [
+  {
+    id: 1,
+    name: "Pallet Town",
+    type: "Town",
+    maxSpawnNumber: 0,
+    buildings: []
+  },
+  {
+    id: 2,
+    name: "Viridian Forest",
+    type: "Forest",
+    maxSpawnNumber: 15,
+    buildings: []
+  },
+  {
+    id: 3,
+    name: "Viridian City",
+    type: "City",
+    maxSpawnNumber: 0,
+    buildings: [
+      {
+        id: 1,
+        region: "Viridian City",
+        type: "Pokemon Center"
+      },
+      {
+        id: 2,
+        region: "Viridian City",
+        type: "Pokemart"
+      },
+      {
+        id: 3,
+        region: "Viridian City",
+        type: "Gym"
+      }
+    ]
+  }
+]
+
+module.exports = {
+  mapRegions: mapRegions
+}
