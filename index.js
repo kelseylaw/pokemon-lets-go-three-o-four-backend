28513d84275be790e79d645b1352ab370d7df2e3
cindy-yhtu <yu-hsin.tu@alumni.ubc.ca>
2019-03-24 01:10:19 -0700

    can do a very basic query on page

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/pokemons', db.getPokemons)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'three',
+  host: 'localhost',
+  database: 'proj304',
+  password: 'p304',
+  port: 5432,
+})
+
+const getPokemons = (request, response) => {
+  pool.query('select * from pokemon', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+  getPokemons,
+}
