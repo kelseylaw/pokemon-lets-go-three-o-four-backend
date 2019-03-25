974d5465d722bb7f8e50f989f4e3ae140a78bcb3
Kelsey Law <kelseyhlaw@gmail.com>
2019-03-25 15:16:08 -0700

    Added more endpoints for player-related queries and map-related queries for index.js. Began reorganization of query files.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const db = require('./queries')
+const playerQueries = require('./query/playerQueries')
 
 app.use(bodyParser.json())
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -15,7 +17,23 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+app.post("/authenticate", playerQueries.authenticateUser)
+
+app.get("/user", playerQueries.getUsers)
+
+app.get("/user/:id", playerQueries.findUserByID)
+
+app.get("/user/:id/pokemons", db.getPokemonsByUserID);
+
+app.post("/user", playerQueries.addNewUser)
+
+app.put("/user/:id", playerQueries.editUserByID)
+
 app.get('/pokemon', db.getPokemons)
+
+app.get("/mapRegion", db.getMapRegions)
+
+app.get("/mapRegion/:name", db.findMapRegion)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- queries.js
+++ queries.js
@@ -1,4 +1,5 @@
 const Pool = require('pg').Pool
+
 const pool = new Pool({
   user: 'three',
   host: 'localhost',
@@ -7,15 +8,43 @@ const pool = new Pool({
   port: 5432,
 })
 
-const getPokemons = (request, response) => {
-  pool.query('select * from pokemon', (error, results) => {
-    if (error) {
-      throw error
+const getPokemons = (req, res) => {
+  pool.query('SELECT * FROM Pokemon', (error, results) => {
+    if (error) throw error
+    res.status(200).json(results.rows)
+  })
+}
+
+const getPokemonsByUserID = (req, res) => {
+  const ownerID = parseInt(req.params.id);
+  pool.query(`SELECT Pokemon.ID, Pokemon.Nickname, Pokemon.PokeDexNum, Pokemon.Status, Pokemon.BattlesDone FROM Pokemon JOIN OwnedBy ON Pokemon.ID = OwnedBy.PokemonID WHERE OwnedBy.OwnerID =  ${ownerID}`, (error, results) => {
+    if (error) throw error
+    res.status(200).json(results.rows)
+  })
+}
+
+const getMapRegions = (req, res) => {
+  pool.query('SELECT * FROM MapRegions', (error, results) => {
+    if (error) throw error
+    res.status(200).json(results.rows)
+  })
+}
+
+const findMapRegion = (req, res) => {
+  const mapRegionName = req.params.name;
+  pool.query(`SELECT * FROM MapRegions WHERE Name = '${mapRegionName}'`, (error, results) => {
+    if (error) throw error;
+    else if (results.rows.length < 1) {
+      // please check formatting of message, and change to consistent formatting if wrong
+      res.status(204).json({"Error": "Map region could not be found!"})
     }
-    response.status(200).json(results.rows)
+    res.status(200).json(results.rows[0])
   })
 }
 
 module.exports = {
   getPokemons,
+  getPokemonsByUserID,
+  getMapRegions,
+  findMapRegion
 }
