a21ba1a7c30e9745bba2f812140af40a89b18837
cindy-yhtu <yu-hsin.tu@alumni.ubc.ca>
2019-03-28 19:55:50 -0700

    fixed heal log

--- query/healQueries.js
+++ query/healQueries.js
@@ -13,13 +13,15 @@ const addHealRecord = (request, response) => {
     const buildingId = request.body.buildingid;
     const playableId = request.body.playableid;
     const date = new Date().toISOString();
-    pool.query('INSERT INTO Heals VALUES ($1, $2, $3, TO_DATE($4, \'YYYY-MM-DD\'))', [pokemonId, buildingId, playableId, date], (error, result) => {
+    getNextID('Heals').then(function (id) {
+        pool.query(`INSERT INTO Heals VALUES (${id}, $1, $2, $3, TO_DATE($4, \'YYYY-MM-DD\'))`, [pokemonId, buildingId, playableId, date], (error, result) => {
         if (error) throw error;
         pool.query('SELECT * FROM Heals WHERE PokemonID = $1 AND BuildingID = $2 AND PlayableID = $3',
             [pokemonId, buildingId, playableId], (error, result) => {
             if (error) throw error;
             response.status(200).json(result.rows[0])
             })
+        })
     })
 };
 
@@ -29,6 +31,20 @@ const getAllHealRecords = (request, response) => {
         response.status(200).json({"data": result.rows});
     })
 };
+
+let getNextID = function (table) {
+    return new Promise(function (resolve, reject) {
+        try {
+            pool.query(`SELECT max(id) FROM ${table}`, (error, results) => {
+                if (error) reject(error);
+                resolve(results.rows[0].max + 1)
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    })
+};
+
 
 module.exports = {
     addHealRecord,
